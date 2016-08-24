5ecae43c56d480e6547cd0041d6c3ac1dd186997
Rick van Rein <rick@openfortress.nl>
2016-08-24 07:56:50 +0100

    Fixes to improve building

--- lib/libtlspool_configvar.c
+++ lib/libtlspool_configvar.c
@@ -4,9 +4,12 @@
  * From: Rick van Rein <rick@openfortress.nl>
  */
 
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <ctype.h>
 
 #include <tlspool/starttls.h>
 
