7ae8d4fb389bb1df65d8b31023aff3ed2cdb58d6
Rick van Rein <rick@openfortress.nl>
2016-09-25 21:09:41 +0100

    Update to Handbook, to make it work on tlspool.readthedocs.io

--- doc/handbook/source/conf.py
+++ doc/handbook/source/conf.py
@@ -41,9 +41,9 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
+    'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages',
+    #NOT-ON-READTHEDOCS# 'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
